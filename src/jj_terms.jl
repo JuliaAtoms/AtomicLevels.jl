0fd94809e563ff6abf571120b4a4213615dfb23f
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-03-21 18:13:32 +1300

    Improve performance of jj-term calculation

--- src/jj_terms.jl
+++ src/jj_terms.jl
@@ -1,31 +1,58 @@
+"""
+    terms(o::RelativisticOrbital, w = 1) -> Vector{HalfInteger}
+
+Returns a sorted list of valid ``J`` values of `w` equivalent jj-coupled particles on
+orbital `o` (i.e. `o^w`).
+
+When there are degeneracies (i.e. multiple states with the same ``J`` and ``M`` quantum
+numbers), the corresponding ``J`` value is repeated in the output array.
+"""
 function terms(orb::RelativisticOrbital, w::Int=one(Int))
     j = kappa_to_j(orb.κ)
-    w <= 2j+1 || throw(ArgumentError("w=$w too large for $orb orbital"))
-
+    0 <= w <= 2*j+1 || throw(DomainError(w, "w must be 0 <= w <= 2j+1 (=$(2j+1)) for j=$j"))
+    # We can equivalently calculate the JJ terms for holes, so we'll do that when we have
+    # fewer holes than particles on this shell
     2w ≥ 2j+1 && (w = convert(Int, 2j) + 1 - w)
+    # Zero and one particle cases are simple special cases
     w == 0 && return [zero(HalfInteger)]
     w == 1 && return [j]
+    # _terms_jw is guaranteed to be in descending order
+    reverse!(_terms_jw(j, w))
+end
 
-    # Forms full Cartesian product of all mⱼ, not necessarily the most
-    # performant method.
-    mⱼs = filter(allunique, collect(allchoices([-j:j for i = 1:w])))
-    MJs = map(x -> reduce(+, x), mⱼs) # TODO: make map(sum, mⱼs) work
-
-    Js = HalfInteger[]
-
-    while !isempty(MJs)
-        # Identify the maximum MJ and associate it with J.
-        MJmax = maximum(MJs)
-        N = count(isequal(MJmax), MJs)
-        append!(Js, repeat([MJmax], N))
-        # Remove the -MJ:MJ series, N times.
-        for MJ = -MJmax:MJmax
-            deleteat!(MJs, findall(isequal(MJ), MJs)[1:N])
+function _terms_jw(j::HalfInteger, w::Integer)
+    j >= 0 || throw(DomainError(j, "j must be positive"))
+    1 <= w <= 2*j+1 || throw(DomainError(w, "w must be 1 <= w <= 2j+1 (=$(2j+1)) for j=$j"))
+    # This works by considering all possible n-particle fermionic product states (Slater
+    # determinants; i.e. each orbital can only appear once) of the orbitals with different
+    # m quantum numbers -- they are just different n-element combinations of the possible
+    # m quantum numbers (m ∈ -j:j).
+    #
+    # Each of the product states is still a J_z eigenstate and the eigenvalue is just a sum
+    # of the J_z eigenvalues of the orbitals. As for every coupled J, we also get M ∈ -J:J,
+    # we can just look at the histogram of all the M quantum numbers to figure out which
+    # J states and how many of them we have.
+    Jmax = j*w
+    NJ = convert(Int, 2*Jmax + 1)
+    hist = zeros(Int, NJ)
+    for c in combinations(HalfInteger(-j):HalfInteger(j), w)
+        M = sum(c)
+        i = convert(Int, M + Jmax) + 1
+        hist[i] += 1
+    end
+    # Go through the histogram to figure out the J terms.
+    jvalues = HalfInteger[]
+    Jmid = div(NJ, 2) + (isodd(NJ) ? 1 : 0)
+    for i = 1:Jmid
+        @assert hist[NJ - i + 1] == hist[i] # make sure that the histogram is symmetric
+        J = convert(HalfInteger, Jmax - i + 1)
+        lastbin = (i > 1) ? hist[i-1] : 0
+        @assert hist[i] >= lastbin
+        for _ = 1:(hist[i]-lastbin)
+            push!(jvalues, J)
         end
     end
-
-    # Do we really want unique here?
-    sort(unique(Js))
+    return jvalues
 end
 
 # This is a workaround until seniority number are implemented for
