d4006ebcc87d7364138d36e206528469701a7e2a
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-03-25 16:29:06 +1300

    Add error checking for term symbols
    It is no longer possible to construct nonsensical term symbols with
    negative L or S quantum numbers. Passing a negative ℓ to
    spectroscopic_label() now throws a more informative error.
    
    Finally, also extend spectroscopic labels all the way to Z.

--- src/common.jl
+++ src/common.jl
@@ -1,6 +1,8 @@
-const spectroscopic = "spdfghiklmnoqrtuv"
-spectroscopic_label(ℓ) =
+const spectroscopic = "spdfghiklmnoqrtuvwxyz"
+function spectroscopic_label(ℓ)
+    ℓ >= 0 || throw(DomainError(ℓ, "Negative ℓ values not allowed"))
     ℓ + 1 ≤ length(spectroscopic) ? spectroscopic[ℓ+1] : "[$(ℓ)]"
+end
 
 # Nicer string representation for rationals
 rs(r::Number) = "$(r)"
