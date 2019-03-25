f065c83f3ed919f5c8b00927ca080eb2c0148f75
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-03-25 16:32:08 +1300

    Enable term symbol tests from Xu2006

--- test/terms.jl
+++ test/terms.jl
@@ -114,22 +114,22 @@ using Test
             p = parity(orb)^occ[1]
             ts = o_ts[2]
 
-            p1 = r"([0-9]+)\(((?:[A-Z][0-9]*)+)\)"
+            p1 = r"([0-9]+)\(((?:(?:[A-Z]|\[[0-9]+\])[0-9]*)+)\)"
             p2 = r"([0-9]+)([A-Z])"
-            toL = s -> something(findfirst(isequal(lowercase(s)[1]), AtomicLevels.spectroscopic), 0) - 1
             ts = map(ts) do t
+                t = replace(t, " " => "")
                 m = match(p1, t)
                 if m != nothing
                     S = (parse(Int, m[1]) - 1)//2
-                    map(eachmatch(r"([A-Z])([0-9]*)", m[2])) do mmm
-                        mm = mmm.match
-                        [Term(toL(mm[1]),S,p)
-                         for j in 1:(length(mm)>1 ? parse(Int, mm[2:end]) : 1)]
+                    map(eachmatch(r"([A-Z]|\[[0-9]+\])([0-9]*)", m[2])) do mm
+                        term = parse(Term, "$(m[1])$(mm[1])")
+                        nterms = isempty(mm[2]) ? 1 : parse(Int, mm[2])
+                        [Term(term.L, term.S, p) for j in 1:nterms]
                     end
                 else
                     m = match(p2, t)
-                    S = (parse(Int, m[1]) - 1)//2
-                    Term(toL(m[2]), S, p)
+                    term = parse(Term, t)
+                    Term(term.L, term.S, p)
                 end
             end
             test_single_orbital_terms(orb, occ, vcat(vcat(ts...)...))
@@ -158,8 +158,19 @@ using Test
         test_orbital_terms("f6" => ["1(S4PD6F4G8H4I7K3L4M2N2Q)", "3(P6D5F9G7H9I6K6L3M3NO)", "5(SPD3F2G3H2I2KL)", "7F"])
         test_orbital_terms("f7" => ["2(S2P5D7F10G10H9I9K7L5M4N2OQ)", "4(S2P2D6F5G7H5I5K3L3MN)", "6(PDFGHI)", "8S"])
         # # Data below are from Xu2006
-        # test_orbital_terms("g9" => [2(S8 P19 D35 F40 G52 H54 I56 K53 L53 M44 N40 O32 Q26 R19 T15 U9 V7 W4 X2 YZ) 4(S6 P16 D24 F34 G38 H40 I42 K39 L35 M32 N26 O20 Q16 R11 T7 U5 V3 WX) 6(S3P3D9F8G12H10I12K9L9M6N6O3Q3RT) 8(PDFGHIKL) 10(S)])
-        # test_orbital_terms("h11" => [2(S36 P107 D173 F233 G283 H325 I353 K370 L376 M371 N357 O335 Q307 R275 T241 U207 V173 W142 X114 Y88 Z68 2150 2236 2325 2417 2511 267 274 282 29 30) 4 (S37 P89 D157 F199 G253 H277 I309 K313 L323 M308 N300 O271 Q251 R216 T190 U155 V131 W101 X81 Y59 Z45 2130 2222 2313 249 255 263 27 28) 6 (S12 P35 D55 F76 G90 H101 I109 K111 L109 M105 N97 O87 Q77 R65 T53 U43 V33 W24 X18 Y12 Z8 215 223 2324) 8(S4 P4 D12 F11 G17 H15 I19 K16 L18 M14 N14 O10 Q10 R6 T6 U3 V3WX) 10(PDFGHIKLMN) 12S])
+        test_orbital_terms("g9" => [
+            "2(S8 P19 D35 F40 G52 H54 I56 K53 L53 M44 N40 O32 Q26 R19 T15 U9 V7 W4 X2 YZ)",
+            "4(S6 P16 D24 F34 G38 H40 I42 K39 L35 M32 N26 O20 Q16 R11 T7 U5 V3 WX)",
+            "6(S3P3D9F8G12H10I12K9L9M6N6O3Q3RT)",
+            "8(PDFGHIKL)", "10(S)"
+        ])
+        test_orbital_terms("h11" => [
+            "2(S36 P107 D173 F233 G283 H325 I353 K370 L376 M371 N357 O335 Q307 R275 T241 U207 V173 W142 X114 Y88 Z68 [21]50 [22]36 [23]25 [24]17 [25]11 [26]7 [27]4 [28]2 [29] [30])",
+            "4(S37 P89 D157 F199 G253 H277 I309 K313 L323 M308 N300 O271 Q251 R216 T190 U155 V131 W101 X81 Y59 Z45 [21]30 [22]22 [23]13 [24]9 [25]5 [26]3 [27] [28])",
+            "6(S12 P35 D55 F76 G90 H101 I109 K111 L109 M105 N97 O87 Q77 R65 T53 U43 V33 W24 X18 Y12 Z8 [21]5 [22]3 [23] [24])",
+            "8(S4 P4 D12 F11 G17 H15 I19 K16 L18 M14 N14 O10 Q10 R6 T6 U3 V3WX)",
+            "10(PDFGHIKLMN)", "12S"
+        ])
 
         @test_throws ArgumentError terms(o"2p", 7)
 
