b989896f6e7f62e9213f14b5ce38dffc50bc14b1
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-08-22 16:04:50 +1200

    Test documenter 0.24

--- docs/make.jl
+++ docs/make.jl
@@ -13,7 +13,38 @@ makedocs(
         "Other utilities" => "utilities.md",
         "Internals" => "internals.md",
     ],
-    assets = ["assets/latex.js"],
+    format = Documenter.HTML(
+        mathengine = MathJax(Dict(
+            :TeX => Dict(
+                :equationNumbers => Dict(:autoNumber => "AMS"),
+                :Macros => Dict(
+                    :defd => "≝",
+                    :ket => ["|#1\\rangle", 1],
+                    :bra => ["\\langle#1|", 1],
+                    :braket => ["\\langle#1|#2\\rangle", 2],
+                    :ketbra => ["|#1\\rangle\\!\\langle#2|", 2],
+                    :matrixel => ["\\langle#1|#2|#3\\rangle", 3],
+                    :vec => ["\\mathbf{#1}", 1],
+                    :mat => ["\\mathsf{#1}", 1],
+                    :conj => ["#1^*", 1],
+                    :im => "\\mathrm{i}",
+                    :operator => ["\\mathfrak{#1}", 1],
+                    :Hamiltonian => "\\operator{H}",
+                    :hamiltonian => "\\operator{h}",
+                    :Lagrangian => "\\operator{L}",
+                    :fock => "\\operator{f}",
+                    :lagrange => ["\\epsilon_{#1}", 1],
+                    :vary => ["\\delta_{#1}", 1],
+                    :onebody => ["(#1|#2)", 2],
+                    :twobody => ["[#1|#2]", 2],
+                    :twobodydx => ["[#1||#2]", 2],
+                    :direct => ["{\\operator{J}_{#1}}", 1],
+                    :exchange => ["{\\operator{K}_{#1}}", 1],
+                    :diff => ["\\mathrm{d}#1\\,", 1]
+                )
+            )
+        ))
+    )
 )
 
 deploydocs(repo = "github.com/JuliaAtoms/AtomicLevels.jl.git")
